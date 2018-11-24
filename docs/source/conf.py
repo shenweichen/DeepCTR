d40152a8a52d56e26de2ff4511dfcb33e444e14f
Weichen Shen <519589356@qq.com>
2018-11-24 22:15:29 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Weichen Shen'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
