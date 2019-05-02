513ece9af001b62f5ee6c82a9fbeb925f3b6a235
“阅谦” <weichen.swc@alibaba-inc.com>
2019-05-02 21:05:49 +0800

    Create run_all.sh

---
+++ examples/run_all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd ..
+pip install deepctr -e .
+cd ./examples
+code_path=./
+
+python $code_path"run_din.py"
+python $code_path"run_classification_criteo.py"
+python $code_path"run_classification_criteo_hash.py"
+python $code_path"run_regression_movielens.py"
+python $code_path"run_multivalue_movielens.py"
+python $code_path"run_multivalue_movielens_hash.py"
+python $code_path"run_dien.py"
+
+
+echo "examples run done!!"
