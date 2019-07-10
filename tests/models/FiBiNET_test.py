b9272b65780069bbd092bf2c3d23ee70aa2951c1
“阅谦” <weichen.swc@alibaba-inc.com>
2019-07-10 09:42:55 +0800

    Update FiBiNET_test.py

--- tests/models/FiBiNET_test.py
+++ tests/models/FiBiNET_test.py
@@ -14,7 +14,7 @@ def test_FiBiNET(bilinear_type):
     sample_size = SAMPLE_SIZE
     x, y, feature_columns = get_test_data(sample_size, 3, 3)
 
-    model = FiBiNET(feature_columns, feature_columns, bilinear_type=bilinear_type,dnn_hidden_units=[32, 32], dnn_dropout=0.5,)
+    model = FiBiNET(feature_columns, feature_columns, bilinear_type=bilinear_type,dnn_hidden_units=[8, 8], dnn_dropout=0.5,)
     check_model(model, model_name, x, y)
 
 
