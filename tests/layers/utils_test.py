acd08c40a72c5758e93b2c84fb11a073958a73b7
charlesdeng <dengc367@gmail.com>
2021-07-01 14:48:40 +0800

    use CustomObjectScope and layer_test

--- tests/layers/utils_test.py
+++ tests/layers/utils_test.py
@@ -1,27 +1,26 @@
+import pytest
 import tensorflow as tf
 from deepctr.layers.utils import Hash
 from deepctr.feature_column import SparseFeat
+from tests.utils import layer_test
+try:
+    from tensorflow.python.keras.utils import CustomObjectScope
+except:
+    from tensorflow.keras.utils import CustomObjectScope
 
 
-def test_hash():
-    vocab_path = "./tests/layers/vocabulary_example.csv"
-    vocab_size = 3+1
-    sf = SparseFeat('user_id', vocab_size, vocabulary_path=vocab_path)
-    hash = Hash(num_buckets=sf.vocabulary_size, vocabulary_path=sf.vocabulary_path, default_value=0)
-    assert hash(tf.constant('lake')) == 1
-    assert hash(tf.constant('johnson')) == 3
-    assert hash(tf.constant('lakemerson')) == 0
-
+@pytest.mark.parametrize(
+    'num_buckets,mask_zero,vocabulary_path,input_data,expected_output',
+    [
+        (3+1, False, None, ['lakemerson'], None),
+        (3+1, True, None, ['lakemerson'], None),
+        (3+1, False, "./tests/layers/vocabulary_example.csv", [['lake'], ['johnson'], ['lakemerson']], [[1], [3], [0]])
+    ]
+)
+def test_Hash(num_buckets, mask_zero, vocabulary_path, input_data, expected_output):
     if tf.version.VERSION < '2.0.0':
         return
-
-    hash_val = tf.strings.to_hash_bucket_fast('lakemerson', vocab_size)
-    hash = Hash(num_buckets=vocab_size)
-    assert hash(tf.constant('lakemerson')) == hash_val
-
-    hash_val = tf.strings.to_hash_bucket_fast('lakemerson', vocab_size-1) + 1
-    hash = Hash(num_buckets=vocab_size, mask_zero=True)
-    assert hash(tf.constant('lakemerson')) == hash_val
-
-    hash = Hash(num_buckets=vocab_size, mask_zero=True)
-    assert hash(tf.constant('0')) == 0
+    with CustomObjectScope({'Hash': Hash}):
+        layer_test(Hash, kwargs={'num_buckets': num_buckets, 'mask_zero': mask_zero, 'vocabulary_path': vocabulary_path},
+                   input_dtype=tf.string, input_data=tf.constant(input_data, dtype=tf.string),
+                   expected_output_dtype=tf.int64, expected_output=expected_output)
