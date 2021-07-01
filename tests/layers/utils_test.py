2cdd1594a1653f4931621422ace7a4b8ab694737
charlesdeng <dengc367@gmail.com>
2021-07-01 22:12:40 +0800

    modify the tf.constant to np.array

--- tests/layers/utils_test.py
+++ tests/layers/utils_test.py
@@ -1,7 +1,7 @@
 import pytest
+import numpy as np
 import tensorflow as tf
 from deepctr.layers.utils import Hash
-from deepctr.feature_column import SparseFeat
 from tests.utils import layer_test
 try:
     from tensorflow.python.keras.utils import CustomObjectScope
@@ -22,5 +22,5 @@ def test_Hash(num_buckets, mask_zero, vocabulary_path, input_data, expected_outp
         return
     with CustomObjectScope({'Hash': Hash}):
         layer_test(Hash, kwargs={'num_buckets': num_buckets, 'mask_zero': mask_zero, 'vocabulary_path': vocabulary_path},
-                   input_dtype=tf.string, input_data=tf.constant(input_data, dtype=tf.string),
+                   input_dtype=tf.string, input_data=np.array(input_data, dtype='str'),
                    expected_output_dtype=tf.int64, expected_output=expected_output)
