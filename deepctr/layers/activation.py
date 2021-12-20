b8e12cb42e1b234942f10fd6c83fdf9fa2c50031
Baoyuan Qi <qibaoyuan@126.com>
2021-12-20 15:39:13 +0800

    instance support for act layer
    instance support for act layer

--- deepctr/layers/activation.py
+++ deepctr/layers/activation.py
@@ -70,6 +70,8 @@ def activation_layer(activation):
         act_layer = tf.keras.layers.Activation(activation)
     elif issubclass(activation, Layer):
         act_layer = activation()
+    elif isinstance(activation, Layer):
+        act_layer = activation
     else:
         raise ValueError(
             "Invalid activation,found %s.You should use a str or a Activation Layer Class." % (activation))
