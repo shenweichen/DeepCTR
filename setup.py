11c37f33e5bed64bd2588492c71008200bca82ff
浅梦 <weichenswc@163.com>
2022-10-15 13:21:13 +0800

    c

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setuptools.setup(
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
